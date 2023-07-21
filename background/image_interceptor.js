8d0364aa21836df0838a53635c38dfe2fdcdb7b8
Perseverant Determination <persevdt+git@gmail.com>
2023-07-21 18:36:01 +0800

    Add additional download options

--- background/image_interceptor.js
+++ background/image_interceptor.js
@@ -1,10 +1,16 @@
-let canvas = new OffscreenCanvas(1000, 1000);
+let canvas = new OffscreenCanvas(3000, 2000);
 let ctx = canvas.getContext("2d");
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 3000, 2000);
 
-const chunkOffset = {
-    "1": { x: 0, y: -500 },
-    "4": { x: 0, y: 500 }
-}
+const chunkOffset = [
+    {x: 0, y: 0},
+    {x: 1000, y: 0},
+    {x: 2000, y: 0},
+    {x: 0, y: 1000},
+    {x: 1000, y: 1000},
+    {x: 2000, y: 1000}
+]
 
 chrome.webRequest.onBeforeRequest.addListener(
     async function (details) {
@@ -13,35 +19,75 @@ chrome.webRequest.onBeforeRequest.addListener(
 
         try {
             const response = await fetch(details.url);
-            if (!response.ok) chrome.tabs.sendMessage(details.tabId, { action: "error", error: error, source: "background/image_interceptor.js" });
+            if (!response.ok) await chrome.tabs.sendMessage(details.tabId, {
+                action: "error",
+                name: response.status,
+                message: response.statusText,
+                source: "background/image_interceptor.js"
+            });
             else {
                 let blob = await response.blob()
-                const parts = /https:\/\/garlic-bread\.reddit\.com\/media\/canvas-images\/(?:.+)\/(.)\/(?:.+)/.exec(details.url);
+                const parts = /https:\/\/garlic-bread\.reddit\.com\/media\/canvas-images\/.+\/(.)\/.+/.exec(details.url);
                 const chunk = parts[1];
 
                 let imageData = await createImageBitmap(blob);
                 ctx.drawImage(imageData, chunkOffset[chunk].x, chunkOffset[chunk].y);
             }
         } catch (error) {
-            chrome.tabs.sendMessage(details.tabId, { action: "error", name: error.name , message: error.message, source: "background/image_interceptor.js" });
+            await chrome.tabs.sendMessage(details.tabId, {
+                action: "error",
+                name: error.name,
+                message: error.message,
+                source: "background/image_interceptor.js"
+            });
         }
 
-        return { cancel: false }
+        return {cancel: false};
     },
     {
         urls: ["https://garlic-bread.reddit.com/media/canvas-images/*/*"],
         types: ["image"]
     },
     []
-)
+);
 
-chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function (msg) {
     if (msg.action === "download") {
-        if (msg.chunk === null) {
-            saveImageData(await canvas.convertToBlob({ type: "image/png" }));
+        let tlx, tly, brx, bry;
+        switch (msg.part) {
+            case "full":
+                [tlx, tly, brx, bry] = [0, 0, 3000, 2000];
+                break;
+            case "whole":
+                [tlx, tly, brx, bry] = [1000, 500, 2000, 1500];
+                break;
+            case "chunk":
+                [tlx, tly, brx, bry] = [
+                    chunkOffset[msg.chunk].x,
+                    chunkOffset[msg.chunk].y,
+                    chunkOffset[msg.chunk].x + 1000,
+                    chunkOffset[msg.chunk].y + 1000,
+                ];
+                break;
+            case "rect":
+                [tlx, tly, brx, bry] = [
+                    msg.tlx + 1500,
+                    msg.tly + 1000,
+                    msg.brx + 1500,
+                    msg.bry + 1000
+                ]
+                break;
         }
+
+        console.table(msg);
+
+        let bufferCanvas = new OffscreenCanvas(brx - tlx, bry - tly);
+        let bufferCtx = bufferCanvas.getContext("2d");
+
+        bufferCtx.drawImage(canvas, -tlx, -tly);
+        saveImageData(await bufferCanvas.convertToBlob({type: "image/png"}));
     }
-})
+});
 
 function saveImageData(imageData) {
     "use strict";
--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,47 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("downloadWhole").addEventListener("click", () => {
-        chrome.runtime.sendMessage({ action: "download", chunk: null })
+    document.getElementById("downloadWhole").addEventListener("click", (event) => {
+        chrome.runtime.sendMessage({
+            action: "download",
+            part: "whole"
+        });
+
+        event.preventDefault();
+    });
+    document.getElementById("downloadFull").addEventListener("click", (event) => {
+        chrome.runtime.sendMessage({
+            action: "download",
+            part: "full"
+        });
+
+        event.preventDefault();
     })
+    for (let i = 0; i < 6; i++) {
+        document.getElementById(`downloadChunk${i}`).addEventListener("click", (event) => {
+            chrome.runtime.sendMessage({
+                action: "download",
+                part: "chunk",
+                chunk: i
+            });
+
+            event.preventDefault();
+        });
+    }
+    document.getElementById("downloadRect").addEventListener("click", (event) => {
+        let tlx = parseInt(document.getElementById("top-left-x").value);
+        let tly = parseInt(document.getElementById("top-left-y").value);
+        let brx = parseInt(document.getElementById("bottom-right-x").value);
+        let bry = parseInt(document.getElementById("bottom-right-y").value);
+        chrome.runtime.sendMessage({
+            action: "download",
+            part: "rect",
+            tlx: tlx,
+            tly: tly,
+            brx: brx,
+            bry: bry
+        });
+
+        event.preventDefault();
+    });
 })
